0dbe5d2b3c7bd8dd70862ba9f8e83e15032cc319
ruscat <rus.reg.36@gmail.com>
2020-01-07 16:21:02 +0300

    28

---
+++ src/components/todo-list.js
@@ -0,0 +1,12 @@
+import  React from 'react';
+import  TodoListItem from './todo-list-item';
+const TodoList = ()=>{
+    const items = ['learn react', 'build awesome app'];
+    return (
+        <ul>
+            <li><TodoListItem/></li>
+            <li><TodoListItem/></li>
+        </ul>
+    );
+};
+export  default  TodoList;
