dade258fe4ecad775579991d962839c6c47044c0
ruscat <rus.reg.36@gmail.com>
2020-01-08 14:12:48 +0300

    39

--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -3,13 +3,14 @@ import  TodoListItem from '../todo-list-item/todo-list-item';
 import './todo-list.css';
 
 
-const TodoList = ({todos})=>{
+const TodoList = ({todos, onDeleted})=>{
     const elements = todos.map((item)=>{
         const {id, ...rest}= item;
         return (
             <li key={id} className='list-group-item'>
                 <TodoListItem
                     {...rest}
+                    onDeleted ={()=>onDeleted(id)}
                 />
             </li>
         );
