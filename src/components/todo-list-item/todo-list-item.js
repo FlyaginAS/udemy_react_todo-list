9bcfe8b853ea40befead6f7ecf08dd405179e74d
ruscat <rus.reg.36@gmail.com>
2020-01-08 12:51:17 +0300

    35

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -2,6 +2,9 @@ import  React from 'react';
 import './todo-list-item.css';
 
 export default class TodoListItem extends  React.Component {
+     onLabelClick = ()=> {
+        console.log(`done: ${this.props.label}`)
+    };
     render(){
         const {label, important = false} = this.props;
         const  style = {
@@ -10,22 +13,23 @@ export default class TodoListItem extends  React.Component {
         };
         return (
             <span className="todo-list-item">
-      <span
-          className="todo-list-item-label"
-          style={style}>
-        {label}
-      </span>
+              <span
+                  className="todo-list-item-label"
+                  style={style}
+              onClick={this.onLabelClick}>
+                {label}
+              </span>
 
-      <button type="button"
-              className="btn btn-outline-success btn-sm float-right">
-        <i className="fa fa-exclamation" />
-      </button>
+              <button type="button"
+                      className="btn btn-outline-success btn-sm float-right">
+                <i className="fa fa-exclamation" />
+              </button>
 
-      <button type="button"
-              className="btn btn-outline-danger btn-sm float-right">
-        <i className="fa fa-trash-o" />
-      </button>
-    </span>
+              <button type="button"
+                      className="btn btn-outline-danger btn-sm float-right">
+                <i className="fa fa-trash-o" />
+              </button>
+        </span>
         );
     }
 }
